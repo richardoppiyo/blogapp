49aed4b8c516467f47153a0832223c4ee21e7225
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-10 00:08:45 +0300

    validations for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ApplicationRecord
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :delete_all
+  has_many :likes, dependent: :delete_all
   belongs_to :author, class_name: 'User'
+
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
 
   def display_comments
     comments.limit(5).order(created_at: :desc)
