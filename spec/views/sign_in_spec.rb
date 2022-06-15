7f99d33f97209ccbeb12ca8172f33a53826c7070
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-15 19:49:19 +0300

    add the username test

---
+++ spec/views/sign_in_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Sign in page', type: :system do
+  describe 'Sign in page' do
+    it 'shows the username field' do
+      visit user_session_path
+      expect(page).to have_content('Email')
+    end
+
+   
+  end
+end
