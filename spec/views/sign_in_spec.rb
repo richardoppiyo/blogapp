bdf84164fb9cdb14f81ae30d78ff3e8eec7d420f
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-15 20:13:49 +0300

    add test to the sign in page

--- spec/views/sign_in_spec.rb
+++ spec/views/sign_in_spec.rb
@@ -28,5 +28,13 @@ RSpec.describe 'Sign in page', type: :system do
         click_on 'Log in'
         expect(page).to have_content('Invalid Email or password')
     end
+
+    it 'click Log in button with correct credential to redirect to root page' do
+        visit user_session_path
+        fill_in 'Email', with: 'richman@gmail.com'
+        fill_in 'Password', with: '345ff'
+        click_on 'Log in'
+        expect(page.current_path).to have_content('/users/')
+    end
   end
 end
