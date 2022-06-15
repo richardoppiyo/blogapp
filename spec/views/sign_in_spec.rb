867bec9d9564c19b3cf5b529a54b27c6ccd7b9c1
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-15 19:49:45 +0300

    add the password field test

--- spec/views/sign_in_spec.rb
+++ spec/views/sign_in_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe 'Sign in page', type: :system do
       expect(page).to have_content('Email')
     end
 
-   
+    it 'shows the Password field' do
+        visit user_session_path
+        expect(page).to have_content('Password')
+    end
+
   end
 end
