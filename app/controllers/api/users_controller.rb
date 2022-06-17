86eccc28f66e9118e21a6dfe2df23864c0777168
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-18 00:32:49 +0300

    check 6

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,13 @@
+class Api::UsersController < ApplicationController
+    # before_action :authenticate_user!
+    def index
+      @users = User.all
+     
+      render json: @users, status: :ok
+    end
+  
+    def show
+      @user = User.find(params[:id])
+    end
+  end
+  
